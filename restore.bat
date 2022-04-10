7875281e1782953b17ad7cd729cb315d5b0ab4c4
LiarOnce <liaronce@hotmail.com>
2022-04-10 20:47:43 +0800

    upload

--- restore.bat
+++ restore.bat
@@ -5,5 +5,5 @@ chcp 65001
 
 echo 正在还原备份,请确保杀毒软件均已放行
 copy /y "backup\hosts.bak" "C:\Windows\System32\drivers\etc\hosts"
-echo 还原完成
+echo 还原完成,若使 GMS2 无法更新请检查 hosts 文件是否有多余的 host 记录,若有删除即可。
 pause
--- start.bat
+++ start.bat
@@ -18,7 +18,7 @@ echo 备份已存在,无需备份。
 echo.
 
 :write_start
-echo 正在添加记录,请确保杀毒软件均已放行
+echo 正在添加 host 记录,请确保杀毒软件均已放行
 echo 127.0.0.1 gms.yoyogames.com >> C:\Windows\System32\drivers\etc\hosts
 echo 添加完成
 echo.
@@ -26,6 +26,9 @@ echo.
 if exist "C:\Windows\System32\curl.exe" goto download_rss
 else if exist ".\curl.exe" goto download_rss_manually_curl
 else goto download_curl_notfound
+
+if exist "htdocs\Zeus-Runtime.rss" del /q "htdocs\Zeus-Runtime.rss"
+if exist "htdocs\Zeus-Runtime-NuBeta.rss" del /q "htdocs\Zeus-Runtime-NuBeta.rss"
 
 :download_rss
 echo 下载镜像站 RSS,将调用系统内置 curl(适用于2022-01更新后的 Windows 10 和 Windows 11)。
@@ -44,7 +47,8 @@ goto run_httpd
 :download_curl_notfound
 echo 内置 curl 不存在,请从 https://curl.se/windows/ 手动下载并提取 curl.exe 到本目录下。
 echo 并重启本脚本
+pause
 
 :run_httpd
-echo 启动 TinyHTTPd,如需关闭可按下 Ctrl + C,若 TinyHTTPd 退出请检查 80 端口是否被占用。
+echo 启动 TinyHTTPd,如需关闭可按下 Ctrl + C,若 TinyHTTPd 提示 startup bind: No error 并退出请检查 80 端口是否被占用：
 tinyhttp
